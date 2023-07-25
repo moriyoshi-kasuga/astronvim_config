c9b611bae9accc3af07a03b60dea5246c715a76c
moriyoshi-kasug <moriyoshi.kasuga1218@gmail.com>
2023-07-26 00:33:44 +0900

    add js and ts

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,7 @@ return {
   -- { import = "astrocommunity.pack.json" },
   -- { import = "astrocommunity.pack.lua" },
   -- { import = "astrocommunity.pack.html-css" },
+  -- { import = "astrocommunity.pack.typescript" },
 
   -- { import = "astrocommunity.project.neoconf-nvim" },
 
