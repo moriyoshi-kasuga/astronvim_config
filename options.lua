ffc50efad353a0b575b44b644bc3ca8da9afc29b
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-08-05 21:09:14 +0900

    add markdown preview option of mermaid default theme to dark

--- options.lua
+++ options.lua
@@ -2,5 +2,10 @@ return {
   g = {
     mkdp_highlight_css = "/home/rainbowmori/.config/nvim/lua/user/markdown-preview/highlight.css",
     mkdp_markdown_css = "/home/rainbowmori/.config/nvim/lua/user/markdown-preview/markdown.css",
+    mkdp_preview_options = {
+      maid = {
+        theme = "dark",
+      },
+    },
   },
 }
