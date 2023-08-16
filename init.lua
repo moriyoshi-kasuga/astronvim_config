ddf76d42caa252fb541be0a6e20c2af3a103a227
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-08-16 23:37:30 +0900

    that at typiscript comminuty plugin pack

--- init.lua
+++ init.lua
@@ -1,28 +1,9 @@
 return {
   vim.cmd "set clipboard+=unnamedplus",
   lsp = {
-    config = {
-      denols = function(opts)
-        opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
-        return opts
-      end,
-      tsserver = function(opts)
-        opts.root_dir = require("lspconfig.util").root_pattern "package.json"
-        return opts
-      end,
-      eslint = function(opts)
-        opts.root_dir = require("lspconfig.util").root_pattern("package.json", ".eslintrc.json", ".eslintrc.js")
-        return opts
-      end,
-    },
     formatting = {
+      format_on_save = true,
       timeout_ms = 5000, -- default format timeout
-      disabled = { "lua_ls", "prettierd" },
-      filter = function(client)
-        if vim.bo.filetype == "html" then return client.name == "null-ls" end
-        if vim.bo.filetype == "javascript" then return client.name == "null-ls" end
-        return true
-      end,
     },
   },
 
@@ -31,45 +12,6 @@ return {
       "williamboman/mason-lspconfig.nvim",
       opts = {
         ensure_installed = { "html-lsp", "ccs-lsp", "djlint" },
-      },
-    },
-    {
-      "jay-babu/mason-null-ls.nvim",
-      opts = {
-        handlers = {
-          -- for prettier
-          prettier = function()
-            require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
-              condition = function(utils)
-                return utils.root_has_file "package.json"
-                  or utils.root_has_file ".prettierrc"
-                  or utils.root_has_file ".prettierrc.json"
-                  or utils.root_has_file ".prettierrc.js"
-              end,
-            })
-          end,
-          -- for prettierd
-          prettierd = function()
-            require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
-              condition = function(utils)
-                return utils.root_has_file "package.json"
-                  or utils.root_has_file ".prettierrc"
-                  or utils.root_has_file ".prettierrc.json"
-                  or utils.root_has_file ".prettierrc.js"
-              end,
-            })
-          end,
-          -- For eslint_d:
-          eslint_d = function()
-            require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
-              condition = function(utils)
-                return utils.root_has_file "package.json"
-                  or utils.root_has_file ".eslintrc.json"
-                  or utils.root_has_file ".eslintrc.js"
-              end,
-            })
-          end,
-        },
       },
     },
   },
