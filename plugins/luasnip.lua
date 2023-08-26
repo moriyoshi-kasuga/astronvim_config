99e278027526ea02eb02a79d2bcf17b436f7a356
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-08-26 11:57:38 +0900

    fix duplicate html

--- plugins/luasnip.lua
+++ plugins/luasnip.lua
@@ -3,10 +3,12 @@ return {
   config = function(plugin, opts)
     -- include the default astronvim config that calls the setup call
     require "plugins.configs.luasnip"(plugin, opts)
-    require("luasnip").filetype_extend("javascript", { "javascriptreact" })
-    require("luasnip").filetype_extend("javascript", { "html" })
+
+    -- require("luasnip").filetype_extend("javascriptreact", { "html" })
+
     require("luasnip").filetype_extend("htmldjango", { "html" })
     require("luasnip.loaders.from_vscode").lazy_load {
+      exclude = { "html" },
       -- this can be used if your configuration lives in ~/.config/nvim
       -- if your configuration lives in ~/.config/astronvim, the full path
       -- must be specified in the next line
