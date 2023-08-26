ed04b7cd09c0345c5883ce20250bd0128bd41b66
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-08-26 18:42:07 +0900

    fix luasnip

--- plugins/luasnip.lua
+++ plugins/luasnip.lua
@@ -1,18 +1,10 @@
 return {
   "L3MON4D3/LuaSnip",
   config = function(plugin, opts)
-    -- include the default astronvim config that calls the setup call
     require "plugins.configs.luasnip"(plugin, opts)
-
-    -- require("luasnip").filetype_extend("javascriptreact", { "html" })
-
-    require("luasnip").filetype_extend("htmldjango", { "html" })
-    require("luasnip.loaders.from_vscode").lazy_load {
-      exclude = { "html" },
-      -- this can be used if your configuration lives in ~/.config/nvim
-      -- if your configuration lives in ~/.config/astronvim, the full path
-      -- must be specified in the next line
-      paths = { "./lua/user/snippets" },
-    }
+    require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } }
+    local luasnip = require "luasnip"
+    luasnip.filetype_extend("javascript", { "javascriptreact" })
+    luasnip.filetype_extend("htmldjango", { "html" })
   end,
 }
