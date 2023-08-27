46f549ae02281cc4a3002a1f4ebc756d61b13751
moriyoshi-kasug <moriyoshi.kasuga1218@gmail.com>
2023-08-27 13:08:25 +0900

    add quick-scope plugin

--- options.lua
+++ options.lua
@@ -1,6 +1,7 @@
 return {
   g = {
     clicpboard = "unnamedplus",
+    qs_highlight_on_keys = { "f", "F", "t", "T" },
     mkdp_highlight_css = "/home/rainbowmori/.config/nvim/lua/user/markdown-preview/highlight.css",
     mkdp_markdown_css = "/home/rainbowmori/.config/nvim/lua/user/markdown-preview/markdown.css",
     mkdp_preview_options = {
