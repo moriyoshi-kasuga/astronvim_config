1017913877e57b851417a977d255c735b7a0d67a
moriyoshi-kasug <moriyoshi.kasuga1218@gmail.com>
2023-09-13 17:47:30 +0900

    Change defaullt hideen item to visible

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,19 @@
 return {
   { "unblevable/quick-scope", lazy = false },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
 }
