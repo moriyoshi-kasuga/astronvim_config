cfe5e6cd3a319389c0418f435685e90e831b50f2
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-09-29 19:53:50 +0900

    Fix js snippets

--- plugins/luasnip.lua
+++ plugins/luasnip.lua
@@ -4,7 +4,6 @@ return {
     require "plugins.configs.luasnip"(plugin, opts)
     require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } }
     local luasnip = require "luasnip"
-    luasnip.filetype_extend("javascript", { "javascriptreact" })
     luasnip.filetype_extend("htmldjango", { "html" })
   end,
 }
