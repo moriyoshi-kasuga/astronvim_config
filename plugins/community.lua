c8fe714cf8aa3c1a7d77bb9baa53d51762e7923d
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-11-09 21:54:44 +0900

    Del: Harpoon Plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -25,7 +25,6 @@ return {
   { import = "astrocommunity.editing-support.neogen" },
 
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.motion.harpoon" },
 
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
 }
