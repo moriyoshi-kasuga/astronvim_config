829ee35c92dae5598a30baa2f9cc78e094267723
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-11-10 22:36:28 +0900

    Add: telescope mapping of close, select next and previous

--- plugins/user.lua
+++ plugins/user.lua
@@ -204,4 +204,17 @@ return {
       }
     end,
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          },
+        },
+      },
+    },
+  },
 }
