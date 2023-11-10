f7d5f29dd8c74b54753132ef7dce74c97bddd47f
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-11-10 10:11:13 +0900

    Fix: duplication indnet of html

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -7,6 +7,10 @@ return {
       if opts.ensure_installed ~= "all" then
         opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html", "css", "groovy" })
       end
+      opts.indent = {
+        enable = true,
+        disable = {"html"}
+      }
     end,
   },
 }
